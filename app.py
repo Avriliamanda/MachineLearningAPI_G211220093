ab739d6725e8525a101328a0fd7343e9a532360e
Maritza Avriliamanda <151169150+Avriliamanda@users.noreply.github.com>
2026-04-21 16:47:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -17,5 +17,26 @@ def predict():
     prediction = model.predict(features)
     return render_template("index.html", prediction_text = "The flower species is {}".format(prediction))
 
+@app.route("/predict", methods=["POST"])
+def predict():
+    try:
+        age = int(request.form['age'])
+        gender = request.form['gender']
+        blood = request.form['blood']
+        condition = request.form['condition']
+
+        data = [[age, gender, blood, condition]]
+        transformed_data = transformer.transform(data)
+
+        prediction = model.predict(transformed_data)
+
+        return render_template("index.html",
+                               prediction_text=f"Predicted Billing Amount: {prediction[0]:.2f}")
+
+    except Exception as e:
+        print("ERROR:", e)  # muncul di logs
+        return render_template("index.html",
+                               prediction_text=f"ERROR: {str(e)}")
+
 if __name__ == "__main__":
     app.run(debug=True)
